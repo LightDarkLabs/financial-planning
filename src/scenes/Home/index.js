db685fd5ed73b3f253ac3a8df83a56961ec91f83
Josh Freeman <jdfreeman11@gmail.com>
2018-08-16 10:30:01 -0500

    Setup example ExpandableTable

--- src/scenes/Home/index.js
+++ src/scenes/Home/index.js
@@ -1,30 +1,44 @@
 import React from 'react'
 import { inject, observer } from 'mobx-react'
+import ExpandableTable from '@components/ExpandableTable'
+
+const TableHeadComponent = () => (
+  <thead>
+    <tr>
+      <th scope="col">Name</th>
+      <th scope="col">Date</th>
+      <th scope="col">Value</th>
+    </tr>
+  </thead>
+)
+
+const RowComponent = ({ item, ...props }) => (
+  <tr {...props}>
+    <td>{item.name || 'asdlfkj'}</td>
+    <td>{item.date.toUTCString()}</td>
+    <td>{item.value.cents}</td>
+  </tr>
+)
+
+const ExpandedRowComponent = ({ item, ...props }) => (
+  <tr {...props}>
+    <td colSpan={3}>
+      <div />
+    </td>
+  </tr>
+)
 
 const Home = ({ store }) => (
   <div>
     <h1>Financial Planning</h1>
 
-    <table className="table">
-      <thead>
-        <tr>
-          <th scope="col">Name</th>
-          <th scope="col">Date</th>
-          <th scope="col">Value</th>
-        </tr>
-
-      </thead>
-
-      <tbody>
-        { store.ItemStore.items.map(item => (
-          <tr key={item.id}>
-            <td>{item.name || 'asdlfkj'}</td>
-            <td>{item.date.toUTCString()}</td>
-            <td>{item.value.cents}</td>
-          </tr>
-          ))}
-      </tbody>
-    </table>
+    <ExpandableTable
+      id="expenses"
+      items={store.ItemStore.items}
+      TableHeadComponent={TableHeadComponent}
+      RowComponent={RowComponent}
+      ExpandedRowComponent={ExpandedRowComponent}
+    />
 
   </div>
 )
